2348a62def6701d2ba84c833a123d780c86fa812
Jeevan Jaison <jeevanjaisonjmj@gmail.com>
2025-10-11 15:36:18 +0530

    Initial Commit

---
+++ Dice Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Project
+//
+//  Created by Jeevan Jaison on 10/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dice ProjectTests/Dice_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_ProjectTests.swift
+//  Dice ProjectTests
+//
+//  Created by Jeevan Jaison on 10/11/25.
+//
+
+import Testing
+@testable import Dice_Project
+
+struct Dice_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
