1e655597b54fee24f9fc799a0921e4b7668cb2d9
Jeevan Jaison <jeevanjaisonjmj@gmail.com>
2025-10-14 21:48:13 +0530

    First manual push

--- Dice Project/ViewController.swift
+++ Dice Project/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //The below code is written by mouse click + control. This is used for creatinng a name for the item
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceImage=0
+    var rightDiceImage=5
+    let diceImages=[UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button Pressed")
+        
+        leftDiceImage=Int.random(in: 0...5)
+        diceImageView1.image=diceImages[leftDiceImage]
+        
+        //dice image view 2
+        rightDiceImage=Int.random(in: 0...5)
+        diceImageView2.image=diceImages[rightDiceImage]
+        
+        
     }
-
-
+    
 }
 
---
+++ Dice Project/playground.swift
@@ -0,0 +1,8 @@
+//
+//  playground.swift
+//  Dice Project
+//
+//  Created by Jeevan Jaison on 10/13/25.
+//
+
+import Foundation
